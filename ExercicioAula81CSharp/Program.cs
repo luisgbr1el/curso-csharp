59ebbb7ac31bf84efd86f1ba62b0aec2ed2f9b6f
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-02-21 13:16:09 -0300

    Create Program.cs

---
+++ ExercicioAula81CSharp/Program.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace ExercicioAula81CSharp {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] line = Console.ReadLine().Split(" ");
+            // Número de linhas
+            int m = int.Parse(line[0]);
+            // Número de colunas
+            int n = int.Parse(line[1]);
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++) {
+                string[] values = Console.ReadLine().Split(" ");
+
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(values[j]);
+
+                }
+            }
+            Console.WriteLine();
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if(mat[i, j] == x) {
+                        Console.WriteLine();
+                        Console.WriteLine("Position " + i + ", " + j + ":");
+
+                        if (j > 0) {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }                        
+                        
+                        if (i > 0) {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+
+                        if (j < n - 1) {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        } 
+
+                        if (i < m - 1) {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
