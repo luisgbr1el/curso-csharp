96b4caee6ae5cfb9e0bb52c3260c33f157d1a83c
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-02-21 13:19:49 -0300

    Create Program.cs

---
+++ ExercicioAula123CSharp/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using ExercicioAula123CSharp.Entities;
+using ExercicioAula123CSharp.Entities.Enums;
+using System.Globalization;
+
+namespace ExercicioAula123CSharp {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(productName, productPrice);
+
+                Console.Write("Quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(productQuantity, productPrice, product);
+                 
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+
+        }
+    }
+}
