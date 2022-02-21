d78ac89a038f66279ae0f0e79d69484964d3a3bf
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-02-21 13:12:24 -0300

    Add files via upload

---
+++ ExercicioAula48CSharp/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioAula48CSharp {
+    class ConversorDeMoeda {
+        public static double Iof = 6.0;
+        public static double Conversor(double dolar, double quantidade) {
+            double convertido = dolar * quantidade;
+            return convertido + convertido * Iof / 100.00;
+        }
+    }
+}
