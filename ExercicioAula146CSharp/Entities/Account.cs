1b706380353e35c0321ac1bf444c29f00401061f
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-02-22 13:56:12 -0300

    Add files via upload

---
+++ ExercicioAula146CSharp/Entities/Account.cs
@@ -0,0 +1,30 @@
+﻿using ExercicioAula146CSharp.Entities.Exceptions;
+
+namespace ExercicioAula146CSharp.Entities {
+    class Account {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account() {
+
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit) {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public double Withdraw(double amount) {
+            if (amount > WithdrawLimit) {
+                throw new WithdrawLimit("The amount exceeds withdraw limit");
+            } else {
+                return Balance -= amount;
+            }
+            
+        }
+    }
+}
---
+++ ExercicioAula146CSharp/Entities/Exceptions/WithdrawLimit.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExercicioAula146CSharp.Entities.Exceptions {
+    class WithdrawLimit : ApplicationException {
+        public WithdrawLimit(string message) : base(message) {
+        }
+    }
+}
